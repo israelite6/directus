fe1c47b29082acd8aa4bf06f0477152d7e5153ec
Israel <israel@Israels-MBP.lan>
2023-09-23 19:54:17 +0100

    initial commit

--- api/src/operations/exec/index.ts
+++ api/src/operations/exec/index.ts
@@ -1,4 +1,6 @@
 import { defineOperationApi } from '@directus/utils';
+import { randUuid } from '@ngneat/falso';
+import axios from 'axios';
 import { createRequire } from 'node:module';
 
 const require = createRequire(import.meta.url);
@@ -16,51 +18,63 @@ function unpackArgs(args: any[]) {
 	return args.length === 1 ? args[0] : args;
 }
 
+// export default defineOperationApi<Options>({
+// 	id: 'exec',
+// 	handler: async ({ code }, { data, env, logger, database, services }) => {
+// 		console.log('first', data);
+// 		const allowedEnv = data['$env'] ?? {};
+// 		const isolateSizeMb = env['FLOWS_RUN_SCRIPT_MAX_MEMORY'];
+// 		const scriptTimeoutMs = env['FLOWS_RUN_SCRIPT_TIMEOUT'];
+
+// 		const isolate = new ivm.Isolate({ memoryLimit: isolateSizeMb });
+// 		const context = isolate.createContextSync();
+// 		const jail = context.global;
+// 		jail.setSync('global', jail.derefInto());
+// 		jail.setSync('process', { env: allowedEnv }, { copy: true });
+// 		jail.setSync('module', { exports: null }, { copy: true });
+
+// 		jail.setSync(
+// 			'console',
+// 			{
+// 				log: new ivm.Callback((...args: any[]) => logger.info(unpackArgs(args)), { sync: true }),
+// 				info: new ivm.Callback((...args: any[]) => logger.info(unpackArgs(args)), { sync: true }),
+// 				warn: new ivm.Callback((...args: any[]) => logger.warn(unpackArgs(args)), { sync: true }),
+// 				error: new ivm.Callback((...args: any[]) => logger.error(unpackArgs(args)), { sync: true }),
+// 				trace: new ivm.Callback((...args: any[]) => logger.trace(unpackArgs(args)), { sync: true }),
+// 				debug: new ivm.Callback((...args: any[]) => logger.debug(unpackArgs(args)), { sync: true }),
+// 			},
+// 			{ copy: true }
+// 		);
+
+// 		// Run the operation once to define the module.exports function
+// 		await context.eval(code, { timeout: scriptTimeoutMs });
+
+// 		const inputData = new ivm.ExternalCopy({ data });
+
+// 		const resultRef = await context.evalClosure(`return module.exports($0.data)`, [inputData.copyInto()], {
+// 			result: { reference: true, promise: true },
+// 			timeout: scriptTimeoutMs,
+// 		});
+
+// 		const result = await resultRef.copy();
+
+// 		// Memory cleanup
+// 		resultRef.release();
+// 		inputData.release();
+// 		context.release();
+// 		isolate.dispose();
+
+// 		// const user = await database.select('*').from('directus_users').where('id', data.$accountability.user);
+// 		console.log('result', result);
+// 		return result;
+// 	},
+// });
 export default defineOperationApi<Options>({
 	id: 'exec',
-	handler: async ({ code }, { data, env, logger }) => {
-		const allowedEnv = data['$env'] ?? {};
-		const isolateSizeMb = env['FLOWS_RUN_SCRIPT_MAX_MEMORY'];
-		const scriptTimeoutMs = env['FLOWS_RUN_SCRIPT_TIMEOUT'];
-
-		const isolate = new ivm.Isolate({ memoryLimit: isolateSizeMb });
-		const context = isolate.createContextSync();
-		const jail = context.global;
-		jail.setSync('global', jail.derefInto());
-		jail.setSync('process', { env: allowedEnv }, { copy: true });
-		jail.setSync('module', { exports: null }, { copy: true });
-
-		jail.setSync(
-			'console',
-			{
-				log: new ivm.Callback((...args: any[]) => logger.info(unpackArgs(args)), { sync: true }),
-				info: new ivm.Callback((...args: any[]) => logger.info(unpackArgs(args)), { sync: true }),
-				warn: new ivm.Callback((...args: any[]) => logger.warn(unpackArgs(args)), { sync: true }),
-				error: new ivm.Callback((...args: any[]) => logger.error(unpackArgs(args)), { sync: true }),
-				trace: new ivm.Callback((...args: any[]) => logger.trace(unpackArgs(args)), { sync: true }),
-				debug: new ivm.Callback((...args: any[]) => logger.debug(unpackArgs(args)), { sync: true }),
-			},
-			{ copy: true }
-		);
-
-		// Run the operation once to define the module.exports function
-		await context.eval(code, { timeout: scriptTimeoutMs });
-
-		const inputData = new ivm.ExternalCopy({ data });
-
-		const resultRef = await context.evalClosure(`return module.exports($0.data)`, [inputData.copyInto()], {
-			result: { reference: true, promise: true },
-			timeout: scriptTimeoutMs,
-		});
-
-		const result = await resultRef.copy();
-
-		// Memory cleanup
-		resultRef.release();
-		inputData.release();
-		context.release();
-		isolate.dispose();
-
-		return result;
+	handler: async ({ code }, { data, env, logger, database, services, accountability }) => {
+		const func = eval(`(${code})`);
+		const t = await func({ data, env, database, accountability, services, randUuid, axios });
+		// const churchId = await database.table('directus_roles').first('church_id').where('id', data.$accountability.role);
+		return t;
 	},
 });
